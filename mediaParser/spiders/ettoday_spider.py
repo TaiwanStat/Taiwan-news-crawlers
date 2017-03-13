f20e960b80941ec98a7f47588d468e2588a07407
Ching <dino8556@gmail.com>
2017-03-13 17:58:05 +0800

    update the ettoday spider due to the update of the website

--- mediaParser/spiders/ettoday_spider.py
+++ mediaParser/spiders/ettoday_spider.py
@@ -8,7 +8,9 @@ from scrapy.selector import Selector
 import time
 import re
 
-TODAY = time.strftime('%m/%d')
+TODAY = time.strftime('%Y/%m/%d')
+TODAY_URL = time.strftime('%Y-%m-%d')
+ROOT_URL = 'http://www.ettoday.net'
 
 
 class EttodaySpider(scrapy.Spider):
@@ -16,56 +18,50 @@ class EttodaySpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://www.ettoday.net/news/news-list.htm'
+            'http://www.ettoday.net/news/news-list-' + TODAY_URL + '-0.htm'
         ]
-
         for url in urls:
-            yield scrapy.Request(url, callback=self.parse_news_list)
+            meta = {'iter_time': 0}
+            yield scrapy.Request(url, callback=self.parse_news_list, meta=meta)
 
     def parse_news_list(self, response):
-        for news_item in response.css('#all-news-list h3'):
+        has_next_page = True
+        response.meta['iter_time'] += 1
+        isFirstIter = response.meta['iter_time'] == 1
+        prefix = '.part_list_2' if isFirstIter else ''
+        for news_item in response.css(prefix+' h3'):
             url = news_item.css('a::attr(href)').extract_first()
+            url = ROOT_URL + url
             category = news_item.css('em::text').extract_first()
             date_time = news_item.css('span::text').extract_first()
 
             if TODAY not in date_time:
+                has_next_page = False
                 continue
 
-            meta = {'category': category}
-            yield scrapy.Request(url, callback=self.parse_news, meta=meta)
+            response.meta['category'] = category
+            yield scrapy.Request(url, callback=self.parse_news, meta=response.meta)
+        if(has_next_page):
+            tFile = time.strftime('%Y%m%d') + '.xml'
+            yield scrapy.FormRequest(url="http://www.ettoday.net/show_roll.php",
+                                     callback=self.parse_news_list,
+                                     meta=response.meta,
+                                     formdata={'offset': str(response.meta['iter_time']),
+                                               'tPage': '3',
+                                               'tFile': tFile,
+                                               'tOt': '0',
+                                               'tSi': '100'
+                                               })
 
-        page_list = []
-        for p in response.css('.menu_page a').extract():
-            sel = Selector(text=p)
-            try:
-                page_list.append({
-                    'num': int(sel.xpath('//text()').extract_first()),
-                    'link': sel.xpath('//@href').extract_first()
-                })
-            except:
-                pass
-
-        current_page = int(response.css('.menu_page .current::text')
-                                   .extract_first())
-
-        max_page_num = page_list[-1]['num']
-        if not page_list or current_page >= max_page_num:
-            return
-
-        next_page = current_page + 1
-
-        for page in page_list:
-            if page['num'] > current_page:
-                url = response.urljoin(page['link'])
-                yield scrapy.Request(url, callback=self.parse_news_list)
-                break
+        # get scroll news list
+        # yield scrapy.Request(url, callback=self.parse_news_list)
 
     def parse_news(self, response):
         title = response.css('h1.title::text').extract_first()
         if not title:
             title = response.css('h2.title::text').extract_first()
             if not title:
-                title = response.css('h1::text').extract_first()
+                title = response.css('h1.title_article::text').extract_first()
 
         p_list = response.css('.story p::text').extract()
 
