125b91d67224ef83b85f86f635aeb7109487f578
Ching <dino8556@gmail.com>
2017-11-27 11:10:21 +0800

    Turn off robot.text obey policy

--- mediaParser/settings.py
+++ mediaParser/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = 'mediaParser.spiders'
 #USER_AGENT = 'mediaParser (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
