645cc5e1daef20b318ff2893c34088c97454c520
Ching <dino8556@gmail.com>
2017-11-27 11:11:45 +0800

    Remove html tags in content, enhance readability

--- mediaParser/spiders/tvbs_spider.py
+++ mediaParser/spiders/tvbs_spider.py
@@ -5,6 +5,7 @@ Usage: scrapy crawl tvbs -o <filename.json>
 """
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 from datetime import date, timedelta
 
 import scrapy
@@ -31,7 +32,8 @@ class TvbsSpider(scrapy.Spider):
         current_page_index = int(url_arr[-1])
 
         if current_page_index < total_pages_num:
-            next_page_url = '/'.join(url_arr[:-1]) + '/' + str(current_page+1)
+            next_page_url = '/'.join(url_arr[:-1]) + \
+                '/' + str(current_page_index + 1)
             yield scrapy.Request(next_page_url, callback=self.parse)
 
     def parse_news(self, response):
@@ -39,14 +41,23 @@ class TvbsSpider(scrapy.Spider):
         date_of_news = response.css('.newsdetail-time1 p::text').extract_first()[:10]
         raw_content = response.css('.newsdetail-content').extract_first()
 
+        TAG_RE = re.compile(r'<[^>]+>([^<]*</[^>]+>)?')
+
         content_prefix = '<!-- 新聞主內容 -->'
-        content_suffix = '<strong>'
+        content_suffix1 = '<strong>'
+        content_suffix2 = '<!--'
 
         content = raw_content.split(content_prefix)[1]
-        content = content.split(content_suffix)[0]
+
+        if content_suffix1 in content:
+            content = content.split(content_suffix1)[0]
+        elif content_suffix2 in content:
+            content = content.split(content_suffix2)[0]
+
         content = content.replace('<br>', ' ')
         content = content.replace('\n', ' ')
         content = content.replace('\t', ' ')
+        content = TAG_RE.sub(' ', content)
         content = content.strip()
 
         yield {
