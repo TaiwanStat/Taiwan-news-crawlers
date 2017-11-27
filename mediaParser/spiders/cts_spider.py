d8f4de8b1043889e22e5038d4a0ecf4a7a4fbccb
Ching <dino8556@gmail.com>
2017-11-27 19:28:40 +0800

    Rename some var, Trim date str (remove time)

--- mediaParser/spiders/cts_spider.py
+++ mediaParser/spiders/cts_spider.py
@@ -20,28 +20,28 @@ class CtsSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.parse_news)
 
         # Auto-parse next page
-        pages_info = response.css('.page-desc::text').extract_first() # 第 XX 頁/共 XX 頁
-        total_pages = pages_info.split('/')[1] #共 XX 頁
+        page_desc = response.css('.page-desc::text').extract_first()
+        total_pages = page_desc.split('/')[1]
         total_pages = int(total_pages[2:-2])
         url_arr = response.url.split('/')
-        current_page_info = url_arr[-1]
-        current_page = current_page_info[5:-5]
-        if current_page is '':
-            current_page = 1
+        url_suffix = url_arr[-1]
+        current_page_index = url_suffix[5:-5]
+        if current_page_index is '':
+            current_page_index = 1
         else :
-            current_page = int(current_page)
+            current_page_index = int(current_page_index)
 
-        if current_page < total_pages:
-            next_page = '/'.join(url_arr[:-1]) + '/index' + str(current_page+1) + '.html'
+        if current_page_index < total_pages:
+            next_page = '/'.join(url_arr[:-1]) + '/index' + str(current_page_index+1) + '.html'
             yield scrapy.Request(next_page, callback=self.parse)
 
     def parse_news(self, response):
         title = response.css('.newsbigtitle::text').extract_first().strip(' \t\n\r')
         date_of_news = response.css('.timebar::text').extract_first().strip(' \t\n\r')
-        date_of_news = date_of_news[:16]
+        date_of_news = date_of_news[:10]
         category = response.css('.active a::text').extract()[-1]
         content = response.css('.newscontents p::text').extract()
-        content = ''.join(content)
+        content = ' '.join(content)
 
         yield {
             'website': "華視",
