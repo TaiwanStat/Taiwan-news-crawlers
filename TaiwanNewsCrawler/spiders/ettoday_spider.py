867ceb9d0ba2d0da341aa7baf1e3b92e55b3f521
cool9203 <octer18@gmail.com>
2022-10-25 17:55:15 +0800

    edit: now can crawl ettoday

--- TaiwanNewsCrawler/spiders/ettoday_spider.py
+++ TaiwanNewsCrawler/spiders/ettoday_spider.py
@@ -4,47 +4,57 @@ Usage: scrapy crawl ettoday -o <filename.json>
 """
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
 import scrapy
+import scrapy.http
+import datetime as dt
+import TaiwanNewsCrawler.utils as utils
 
-TODAY = time.strftime('%Y/%m/%d')
-TODAY_URL = time.strftime('%Y-%m-%d')
+
 ROOT_URL = 'https://www.ettoday.net'
+PAGE_URL = "https://www.ettoday.net/news/news-list-{}-0.htm"
+API_URL = "https://www.ettoday.net/show_roll.php"
 
 
 class EttodaySpider(scrapy.Spider):
     name = "ettoday"
 
+    def __init__(self, start_date: str=None, end_date: str=None):
+        super().__init__(start_date=start_date, end_date=end_date)
+
     def start_requests(self):
-        urls = [
-            'https://www.ettoday.net/news/news-list-' + TODAY_URL + '-0.htm'
-        ]
-        for url in urls:
-            meta = {'iter_time': 0}
+        start_date, end_date = utils.parse_start_date_and_end_date(self.start_date, self.end_date)
+        date = start_date
+
+        while (date < end_date):
+            meta = {'iter_time': 0, "date": date, "start_date": date, "end_date": date+dt.timedelta(days=1)}
+            url = PAGE_URL.format(date.strftime("%Y-%m-%d"))
             yield scrapy.Request(url, callback=self.parse_news_list, meta=meta)
+            date += dt.timedelta(days=1)
 
     def parse_news_list(self, response):
-        has_next_page = True
+        start_date, end_date = response.meta["start_date"], response.meta["end_date"]
+        crawl_next = False
         response.meta['iter_time'] += 1
         is_first_iter = response.meta['iter_time'] == 1
         prefix = '.part_list_2' if is_first_iter else ''
-        for news_item in response.css(prefix + ' h3'):
-            url = news_item.css('a::attr(href)').extract_first()
+        date_str = response.meta["date"].strftime("%Y/%m/%d")
+        for news in response.css(prefix + ' h3'):
+            url = news.css('a::attr(href)').extract_first()
             url = ROOT_URL + url
-            category = news_item.css('em::text').extract_first()
-            date_time = news_item.css('span::text').extract_first()
+            category = news.css('em::text').extract_first()
+            news_date = utils.parse_date(news.css('span::text').extract_first())
+            crawl_next = utils.can_crawl(news_date, start_date, end_date)
 
-            if TODAY not in date_time:
-                has_next_page = False
-                continue
+            if (crawl_next):
+                response.meta['category'] = category
+                yield scrapy.Request(
+                    url, callback=self.parse_news, meta=response.meta)
 
-            response.meta['category'] = category
-            yield scrapy.Request(
-                url, callback=self.parse_news, meta=response.meta)
-        if has_next_page:
-            tFile = time.strftime('%Y%m%d') + '.xml'
+        if (crawl_next):
+            date_str = response.meta["date"].strftime("%Y%m%d")
+            tFile = f"{date_str}-1.xml"
             yield scrapy.FormRequest(
-                url="https://www.ettoday.net/show_roll.php",
+                url=API_URL,
                 callback=self.parse_news_list,
                 meta=response.meta,
                 formdata={
@@ -52,12 +62,14 @@ class EttodaySpider(scrapy.Spider):
                     'tPage': '3',
                     'tFile': tFile,
                     'tOt': '0',
-                    'tSi': '100'
+                    'tSi': '100',
+                    "tAr": "0"
                 })
 
 
     def parse_news(self, response):
         title = response.css('h1.title::text').extract_first()
+        date = response.meta["date"].strftime("%Y-%m-%d")
         if not title:
             title = response.css('h2.title::text').extract_first()
             if not title:
@@ -69,11 +81,20 @@ class EttodaySpider(scrapy.Spider):
         for p in p_list:
             content += p
 
+        category = response.meta['category']
+
+        # description
+        try:
+            description = response.css("meta[property='og:description']::attr(content)").extract_first()
+        except:
+            description = ""
+
         yield {
             'website': "東森新聞雲",
             'url': response.url,
             'title': title,
-            'date': time.strftime('%Y-%m-%d'),
+            'date': date,
             'content': content,
-            'category': response.meta['category']
+            'category': category,
+            "description": description
         }
